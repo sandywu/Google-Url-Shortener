5c1b3519495ba9d93e2b6e67161b9ad165cb0171
Sandy <wojiaoabin@gmail.com>
2011-01-23 22:13:32 +0800

    WooHoo! Google Url Shortener FTWgit status

---
+++ script/background.js
@@ -0,0 +1,119 @@
+/*
+ * Google Url Shortener
+ * Background.js
+ * @Author: Sandy
+ * @Date: 2011-01-13
+ * @Email: wojiaoabin@gmail.com
+**/
+
+var xhr = new XMLHttpRequest(), 
+	RESTURI = 'https://www.googleapis.com/urlshortener/v1',
+	KEY = 'AIzaSyBX4LLI6r_U301NYpTkUEbZCO_zvdNM-dM',
+	METHOD = 'POST',
+	requestUrl = RESTURI + '/url',
+	threshold = 8000, cntTabId,
+	timer = null,
+	oauth = ChromeExOAuth.initBackgroundPage({
+	  'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+	  'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+	  'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+	  'consumer_key': 'anonymous',
+	  'consumer_secret': 'anonymous',
+	  'scope': 'https://www.googleapis.com/auth/urlshortener',
+	  'app_name': 'Google URL Shortener'
+	});
+
+
+function onRequest(request, sender, callback) {
+	asyncRequest();
+}
+
+function asyncRequest(longUrl) {
+
+	clearTimeout(timer);
+	queryStr = {'key': KEY};
+
+	params = JSON.stringify({'longUrl': longUrl});
+
+	xhr.open(METHOD, requestUrl + '?' + serialize(queryStr), true);
+	xhr.onreadystatechange = function() {
+	  if(xhr.readyState == 4) {
+		responseHandle(xhr);
+	  }
+	}
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.setRequestHeader('Authorization', oauth.getAuthorizationHeader(requestUrl, METHOD, queryStr));
+	xhr.send(params);
+	xhr.finished = false;
+
+	timer = setTimeout(timeout, threshold);
+	
+}
+
+function responseHandle(xhr) {
+
+	var response = JSON.parse(xhr.responseText),
+		status = xhr.status;
+	
+	if (status == 200 && 'id' in response) {
+		success(response.id);
+	}
+	
+	if ('error' in response) {
+		error();
+	}
+}
+
+function error(msg) {
+	xhr.finished = true;
+	chrome.tabs.sendRequest(cntTabId, {'message': 'error'});
+}
+
+function success(url) {
+	xhr.finished = true;
+	chrome.tabs.sendRequest(cntTabId, {'message': 'success', 'url': url});
+	copyToClipBoard(url);
+}
+
+function timeout() {
+	if (xhr.finished == false) {
+		chrome.tabs.sendRequest(cntTabId, {'message': 'timeout'});
+	}
+}
+
+function serialize(parameters) {
+
+  var params = [];
+
+  for(var p in parameters) {
+	params.push(encodeURIComponent(p) + '=' + encodeURIComponent(parameters[p]));
+  }
+
+  return params.join('&');
+}
+
+function copyToClipBoard(url) {
+
+	var input = document.getElementById('url');
+	
+	input.value = url;					
+	input.select();
+
+	document.execCommand('Copy');
+}
+
+function initialize() {
+
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		  cntTabId = tab.id
+		  chrome.tabs.sendRequest(cntTabId, {'message': 'shorten'}, function(longUrl) {
+				oauth.authorize(function() {
+					asyncRequest(longUrl);
+				});		
+		  });
+	});	
+
+	chrome.extension.onRequest.addListener(onRequest);
+}
+
+initialize();
---
+++ script/content.js
@@ -0,0 +1,124 @@
+/*
+ * Google Url Shortener
+ * Content.js
+ * @Author: Sandy
+ * @Date: 2011-01-13
+ * @Email: wojiaoabin@gmail.com
+**/
+
+;(function(win) {
+	
+	var d = win.document,
+		reSpinUrl = '/images/spin.gif';
+		spinUrl = chrome.extension.getURL(reSpinUrl),
+		imgStr = '<img style="position:absolute;left:15px;top:5px;" src="' + spinUrl + '" />';
+
+	var Tooltip = new function() {
+
+		var tip = d.createElement('div'),
+			cssText = 'text-align:center;position:absolute;font-weight:normal;font-family:"Microsoft Yahei","Arial","Tahoma","Verdana";left:5px;top:5px;display:none;width:220px;color:#fff;height:25px;border:none;line-height:27px;font-size:12px;background:#fff;',
+			timer = null,
+			latency = 3500,
+			bgColor = {
+				'process': '#114BBF',
+				'error': '#EC3200',
+				'timeout': '#EC3200',
+				'success': '#019040'
+			}
+
+		function _text(msg) {
+			tip.innerHTML = msg;
+		}
+
+		function _delay() {
+			clearTimeout(timer);
+			timer = setTimeout(function() {
+				_hide();
+			}, latency);
+		}
+
+		function _changeBg(color) {
+			tip.style.backgroundColor = color;
+		}
+
+		function _show() {
+			tip.style.display = 'block';
+		}
+
+		function _hide() {
+			tip.style.display = 'none';
+		}
+
+		function _init() {
+			d.body.appendChild(tip);
+			tip.setAttribute('style', cssText);
+		}
+
+		this.error = function error() {
+			_text('Error raised!');
+			_delay();
+			_changeBg(bgColor[arguments.callee.name]);
+		}
+
+		this.success = function success() {
+			_text('Copied to clipboard');
+			_delay();
+			_changeBg(bgColor[arguments.callee.name]);
+		}
+
+		this.timeout = function timeout() {
+			_text('Request time out');
+			_delay();
+			_changeBg(bgColor[arguments.callee.name]);
+		}
+
+		this.process = function process() {
+			_show();
+			_text(imgStr + 'Processing your request...');
+			_changeBg(bgColor[arguments.callee.name]);
+		}
+
+		_init();
+
+	}
+
+
+	function onRequest(request, sender, callback) {
+
+		var msg = request.message,
+			longUrl = win.location.href;
+		
+		switch(msg) {
+			case 'shorten': callback(longUrl); Tooltip.process();
+				break;
+			case 'success': success(request.url);
+				break;
+			case 'error': error();
+				break;
+			case 'timeout': timeout();
+				break;
+			default:break;
+		}
+	}
+
+	function success() {
+		Tooltip.success();
+	}
+
+	function error() {
+		Tooltip.error();
+	}
+
+	function timeout() {
+		Tooltip.timeout();
+	}
+
+	function $(elem) {
+		return typeof elem == 'string' ? d.getElementById(elem) : elem;
+	}
+
+	chrome.extension.onRequest.addListener(onRequest);
+
+
+}).apply(null, [this]);
+
